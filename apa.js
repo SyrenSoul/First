d417412af13dbfdcd173d80e96b95afb4a4973c9
Syahid Ashabul Fikri <130534771+SyrenSoul@users.noreply.github.com>
2023-04-13 13:15:36 +0700

    Update apa.js

--- apa.js
+++ apa.js
@@ -1,3 +1,9 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Hello there! Welcome to my page!";
   }
+
+  $(document).ready(function () {
+    $(".menu-toggle").click(function () {
+        $('nav').toggleClass('active');
+    })
+})
